5c0d228294ee6a565099c64ee010feb5662c250a
dzx <dzxdetianxuan@outlook.com>
2025-04-27 11:41:32 +0800

    1.7完成了动态路由的删除接口

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -1,3 +1,17 @@
-export async function POST(request:Request) {
-    console.log('hahahahaha')
+import { NextResponse } from "next/server"
+import { Params } from "./type"
+import db from "@/db"
+
+
+export async function DELETE(request:Request,{params}: {params: Params}) {
+    await db.update(({posts}) => {
+        const idx = posts.findIndex(item => item.id === params.id)
+        posts.splice(idx,1)
+    })
+    return NextResponse.json(
+        {
+            code: 1,
+            message: '删除成功'
+        } 
+    )
 }
