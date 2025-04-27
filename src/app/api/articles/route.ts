8a912d412346bf32335a7a262f9672e2fdc54b18
dzx <dzxdetianxuan@outlook.com>
2025-04-27 21:28:44 +0800

    暂存版本

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,5 +1,27 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import db from "@/db"
+
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const pagenum = Number(searchParams.get('pagenum')) || 1
+    const pagesize = Number(searchParams.get('pagesize')) || 2
+    const query = searchParams.get('query') || ''
+
+    const data = db.data.posts
+
+    let filteredData = query ? data.filter(item => {
+        const {id,...rest} = item
+        return Object.values(rest).some(value => String (value).toLowerCase().includes(query.toLowerCase()))
+    }): data
+
+    const total = filteredData.length
+
+    const startIndex = (pagenum - 1) * pagesize
+    const endIndex = Math.min(startIndex + pagesize, total)
+}
+
+
 
 export async function POST(request:Request) {
     try {
