cd7d2ff687635a8f9721ef9a9a06d9303a53127c
dzx <dzxdetianxuan@outlook.com>
2025-04-28 16:29:53 +0800

    1.9完成了分页查询

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -2,23 +2,59 @@ import { NextRequest, NextResponse } from "next/server"
 import db from "@/db"
 
 
+
+// 分页查询
 export async function GET(request: NextRequest) {
+    
+    // 获取参数
     const searchParams = request.nextUrl.searchParams
     const pagenum = Number(searchParams.get('pagenum')) || 1
     const pagesize = Number(searchParams.get('pagesize')) || 2
     const query = searchParams.get('query') || ''
 
-    const data = db.data.posts
+    // 提前声明try中隔绝的外界需要的变量
+    let filteredData = []
+    let total = 0
+    try {
+        const data = db.data.posts
 
-    let filteredData = query ? data.filter(item => {
-        const {id,...rest} = item
-        return Object.values(rest).some(value => String (value).toLowerCase().includes(query.toLowerCase()))
-    }): data
+        // 从data中筛选出包含query的项（忽略大小写，忽略id）
+        const cfilteredData = query ? data.filter(item => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const {id,...rest} = item
+            return Object.values(rest).some(value => String (value).toLowerCase().includes(query.toLowerCase()))
+        }): data
 
-    const total = filteredData.length
+        // 计算数据量及起始索引及结束索引并最终计算出查找的数据返回给前端
+        const ctotal = filteredData.length
+        // 使变量不可重新赋值
+        filteredData = cfilteredData
+        total = ctotal
+    }catch (error) {
+        console.log('获取失败：',error)
+        return NextResponse.json(
+        {
+            code: 400,
+            message: '获取失败',
+        },
+        {
+            status: 500,
+        })
+    }
 
     const startIndex = (pagenum - 1) * pagesize
     const endIndex = Math.min(startIndex + pagesize, total)
+
+    filteredData = startIndex >= total ? [] : filteredData.slice(startIndex,endIndex)
+
+    return NextResponse.json({
+        code: 200,
+        message: '获取成功',
+        data: {
+            total,
+            list: filteredData
+        }
+    })
 }
 
 
