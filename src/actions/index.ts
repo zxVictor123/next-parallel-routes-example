97b5fd99df3787ec1494b0227d6d6ccc54e70471
dzx <dzxdetianxuan@outlook.com>
2025-05-06 19:14:56 +0800

    1.6使用zod完成了校验

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,7 +2,10 @@
 
 import { revalidatePath } from "next/cache"
 import { sleep } from "@/app/utils/sleep"
+import { z } from 'zod'
 
+ 
+const schema = z.string().min(2, { message: "Must be 2 or more characters long" }).max(5, { message: "Must be 5 or fewer characters long"});
 
 const data = ['吃饭','睡觉','打豆豆']
 
@@ -14,6 +17,13 @@ export async function getTodos() {
 export async function addTodo(prevState: {message: string},formData: FormData) {
     await sleep(2000)
     const todo = formData.get('todo') as string
+    const validatedFields = schema.safeParse(todo)
+// Return early if the form data is invalid
+if (!validatedFields.success) {
+    return {
+        message: validatedFields.error.flatten().formErrors.toString(),
+    }
+}
     data.push(todo)
     revalidatePath('/')
     return {
